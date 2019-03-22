87532848c4ff7d08e8a1657e6984c28464c86464
bhimeshchauhan <bhimeshchauhan@gmail.com>
2019-03-22 18:04:17 -0500

    remove old code
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- window.py
+++ window.py
@@ -31,8 +31,6 @@ class App(QDialog):
         layout = QGridLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
-        # layout.setColumnStretch(1, 4)
-        # layout.setColumnStretch(2, 4)
         count = 0
         for x in range(10):
             for y in range(10):
@@ -46,56 +44,10 @@ class App(QDialog):
                                      "border-radius: 0;"
                                      "padding: 6px;")
                 layout.addWidget(button, x, y)
-                # grid_layout.addWidget(button, x, y)
 
-        # layout.addWidget(QPushButton('1'), 0, 0)
-        # layout.addWidget(QPushButton('2'), 0, 1)
-        # layout.addWidget(QPushButton('3'), 0, 2)
-        # layout.addWidget(QPushButton('4'), 1, 0)
-        # layout.addWidget(QPushButton('5'), 1, 1)
-        # layout.addWidget(QPushButton('6'), 1, 2)
-        # layout.addWidget(QPushButton('7'), 2, 0)
-        # layout.addWidget(QPushButton('8'), 2, 1)
-        # layout.addWidget(QPushButton('9'), 2, 2)
         self.horizontalGroupBox.setLayout(layout)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = App()
     sys.exit(app.exec_())
-
-# import sys
-# from PyQt5.QtWidgets import (QWidget, QGridLayout, QPushButton, QApplication)
-#
-#
-# class basicWindow(QWidget):
-#     def __init__(self):
-#         super().__init__()
-#         grid_layout = QGridLayout()
-#         # grid_layout.setContentsMargins(0, 0, 0, 0)
-#         grid_layout.setSpacing(0)
-#         self.setLayout(grid_layout)
-#         # self.setGeometry(0,0,250,250)
-#         count = 0
-#         for x in range(10):
-#             for y in range(10):
-#                 count += 1
-#                 button = QPushButton(str(count))
-#
-#                 button.setContentsMargins(0,0,0,0)
-#                 button.setStyleSheet(
-#                                      "border-width: 2px; "
-#                                      "border-style: solid;"
-#                                      "border-color: black;"
-#                                      "border-radius: 0;"
-#                                      "padding: 6px;")
-#                 grid_layout.addWidget(button, x, y)
-#
-#         self.setWindowTitle('Basic Grid Layout')
-#
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     windowExample = basicWindow()
-#     windowExample.show()
-#     sys.exit(app.exec_())
