94364d3e6c582b76932b012408bbe1a626018a80
bhimeshchauhan <bhimeshchauhan@gmail.com>
2019-03-24 00:51:48 -0500

    added complex ui
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- window.py
+++ window.py
@@ -19,9 +19,15 @@ class App(QDialog):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
         self.createGridLayout()
         windowLayout = QVBoxLayout()
         windowLayout.addWidget(self.horizontalGroupBox)
+        # self.setLayout(windowLayout)
+
+        self.createGridLayout2()
+        # windowLayout2 = QVBoxLayout()
+        windowLayout.addWidget(self.horizontalGroupBox2)
         self.setLayout(windowLayout)
 
         self.show()
@@ -44,8 +50,29 @@ class App(QDialog):
                                      "border-radius: 0;"
                                      "padding: 6px;")
                 layout.addWidget(button, x, y)
-
         self.horizontalGroupBox.setLayout(layout)
+
+    def createGridLayout2(self):
+        self.horizontalGroupBox2 = QGroupBox()
+        layout2 = QGridLayout()
+        layout2.setContentsMargins(0, 0, 0, 0)
+        layout2.setSpacing(0)
+        count = 0
+        for x in range(10):
+            for y in range(10):
+                count += 1
+                button2 = QPushButton(str(count))
+                button2.setContentsMargins(0, 0, 0, 0)
+                button2.setStyleSheet(
+                    "border-width: 2px; "
+                    "border-style: solid;"
+                    "border-color: black;"
+                    "border-radius: 0;"
+                    "padding: 6px;")
+                layout2.addWidget(button2, x, y)
+        self.horizontalGroupBox2.setLayout(layout2)
+
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
