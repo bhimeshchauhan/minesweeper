43ce025959c3b6d5938668107abb00025e89d753
bhimeshchauhan <bhimeshchauhan@gmail.com>
2019-03-27 09:37:58 -0500

    added matrix to randomize bombs
    Signed-off-by: bhimeshchauhan <bhimeshchauhan@gmail.com>

--- window.py
+++ window.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import QIcon, QColor, QPixmap
 from PyQt5.QtCore import pyqtSlot, Qt
 from qtconsole.qt import QtGui
 from functools import partial
-
+import random
 
 class App(QDialog):
 
@@ -18,6 +18,70 @@ class App(QDialog):
         self.width = 625
         self.height = 625
         self.initUI()
+
+    def findsum(self, candidates, target, maxsize, width):
+        res = []
+        candidates.sort()
+        self.dfs(candidates, target, 0, [], res, maxsize, width)
+        return random.choice(res)
+
+    def dfs(self, nums, target, index, path, res, maxsize, width):
+        if target < 0:
+            return  # backtracking
+        if target == 0:
+            if len(path) <= maxsize:
+                if all(i <= width for i in path):
+                    res.append(path)
+                return
+        for i in range(index, len(nums)):
+            self.dfs(nums, target - nums[i], i, path + [nums[i]], res, maxsize, width)
+
+    def replaceCount(self, matrix, width, height):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[i])):
+                if matrix[i][j] != '*':
+                    # i-1
+                    if i - 1 >= 0 and matrix[i - 1][j] == "*":
+                        matrix[i][j] += 1
+                    # i+1
+                    if i + 1 < height and matrix[i + 1][j] == "*":
+                        matrix[i][j] += 1
+                    # j+1
+                    if j + 1 < width and matrix[i][j + 1] == "*":
+                        matrix[i][j] += 1
+                    # j-1
+                    if j - 1 >= 0 and matrix[i][j - 1] == "*":
+                        matrix[i][j] += 1
+                    # i-1 j-1
+                    if j - 1 >= 0 and i - 1 >= 0 and matrix[i - 1][j - 1] == "*":
+                        matrix[i][j] += 1
+
+                    # i+1 j+1
+                    if j + 1 < width and i + 1 < height and matrix[i + 1][j + 1] == "*":
+                        matrix[i][j] += 1
+                    # i-1 j+1
+                    if j + 1 < width and i - 1 >= 0 and matrix[i - 1][j + 1] == "*":
+                        matrix[i][j] += 1
+                    # i+1 j-1
+                    if j - 1 >= 0 and i + 1 < height and matrix[i + 1][j - 1] == "*":
+                        matrix[i][j] += 1
+        return matrix
+
+    def main(self, width, height, num):
+        if num > height * width:
+            print("Board exploded: Max mines reached")
+            return
+        buckets = [[0 for col in range(width)] for row in range(height)]
+        freq = list(self.findsum(list(range(1, 9)), num, height, width))
+        # print("freq",freq)
+        for i in range(len(freq)):
+            changefor = random.sample(list(enumerate(buckets[i])), freq[i])
+            for j in changefor:
+                buckets[i][j[0]] = "*"
+        mat = self.replaceCount(buckets, width, height)
+        for item in mat:
+            print(''.join(str(x) for x in item))
+        return mat
 
     def meme(self, name):
         print("awesome", name)
@@ -34,20 +98,22 @@ class App(QDialog):
         self.show()
 
     def createGridLayout(self):
+        height = 4
+        width = 4
+        maxbombs = 5
+        mat = self.main(width, height, maxbombs)
         self.horizontalGroupBox = QGroupBox()
         # self.horizontalGroupBox.setContentsMargins(0, 0, 0, 0)
         layout = QGridLayout()
         layout.setContentsMargins(0, 0, 0, 0)
         layout.setSpacing(0)
-        count = 0
-        height = 8
-        width = 10
+        # count = 0
         parentwidth = layout.geometry()
         print( parentwidth, self.width)
         for x in range(height):
             for y in range(width):
-                count += 1
-                button = QPushButton(str(count))
+                # count += 1
+                button = QPushButton(str(mat[x][y]))
                 button.setContentsMargins(0,0,0,0)
                 button.setFixedHeight(self.height // height)
                 button.setCursor(QtGui.QCursor(Qt.PointingHandCursor))
